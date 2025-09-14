6987c69b9110f6a5af5a33cf67a97b9dd06ef48d
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2025-09-14 19:28:31 +0300

    generaattori

---
+++ service-worker_prot.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'tavupeli-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index_prot.html',
+  '/sallitut_sanat.txt',
+  '/manifest_prot.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
